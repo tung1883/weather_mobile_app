6d74d043acd24f3cc38cf5d62cfba8078aa2d56a
tung1883 <nguyenductung1883@gmail.com>
2024-03-25 00:20:07 +0700

    update toogle theme function

--- components/ColorContext.js
+++ components/ColorContext.js
@@ -1,22 +1,60 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { Appearance } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const ColorContext = createContext();
 
 export const ColorProvider = ({ children }) => {
+  const [isAuto, setIsAuto] = useState()
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // useEffect(() => {
-  //   // Load theme preference from AsyncStorage
-  //   AsyncStorage.getItem('darkModeEnabled').then((value) => {
-  //     setIsDarkMode(value === 'true');
-  //   });
-  // }, []);
+  useEffect(() => {
+    const getColorTheme = async () => {
+      let theme = await AsyncStorage.getItem('theme')
 
-  const toggleTheme = async () => {
-    const newMode = !isDarkMode;
-    setIsDarkMode(newMode);
-    await AsyncStorage.setItem('darkModeEnabled', JSON.stringify(newMode));
+      if (!theme) {
+        AsyncStorage.setItem('theme', 'auto')
+        theme = 'auto'
+      }
+  
+      if (theme === 'auto') {
+        setIsAuto(true)
+        if (Appearance.getColorScheme() === 'light') setIsDarkMode(false)
+        else setIsDarkMode(true)
+        return
+      }
+  
+      if (theme === 'light') setIsDarkMode(false)
+      else setIsDarkMode(true)
+    }
+
+    getColorTheme()
+  }, []);
+
+  useEffect(() => {
+    let subscription = null 
+
+    if (isAuto) {
+      subscription = Appearance.addChangeListener(({ colorScheme }) => {
+        if (colorScheme === 'light') setIsDarkMode(false)
+        else setIsDarkMode(true)
+      });
+    }
+
+    return () => subscription?.remove();
+  }, [isAuto]);
+
+
+  const toggleTheme = async (theme) => {
+    if (theme === 'auto') {
+      setIsAuto(true)
+      if (Appearance.getColorScheme() === 'light') setIsDarkMode(false)
+      else setIsDarkMode(true)
+    }
+
+    if (theme === 'light') setIsDarkMode(false)
+    else setIsDarkMode(true)
+    AsyncStorage.setItem('theme', theme)
   };
 
   return (
